824d97508eac92d3db1030dce1d6c5d6dd4d4b98
lucid_dreamer <impera.et.conquer@gmail.com>
2017-09-19 03:56:24 +0300

    Spring Boot Skeleton

---
+++ src/main/java/eu/lucid/HospitalSpringApplication.java
@@ -0,0 +1,12 @@
+package eu.lucid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospitalSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HospitalSpringApplication.class, args);
+	}
+}
